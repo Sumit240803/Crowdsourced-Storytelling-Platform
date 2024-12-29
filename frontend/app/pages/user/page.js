07aa6c4bfac871d51e7694e07cee295ab5e55b3c
Sumit240803 <goyalsumit651@gmail.com>
2024-12-29 21:27:57 +0530

    Frontend

--- frontend/app/pages/user/page.js
+++ frontend/app/pages/user/page.js
@@ -5,6 +5,10 @@ import useAuth from "@/app/hooks/useAuth";
 import Image from "next/image";
 import Nav from "@/app/components/profile/Nav";
 import { MdOutlineEmail } from "react-icons/md";
+import { SiStorybook } from "react-icons/si";
+import { TiGroupOutline } from "react-icons/ti";
+import { FaPeopleRoof } from "react-icons/fa6";
+import { FaPeopleGroup } from "react-icons/fa6";
 
 const User = () => {
     const showAlert = useAuth();
@@ -37,39 +41,50 @@ const User = () => {
         <div>
             <Nav/>
             {/* User Profile */}
-            <div className="flex flex-col md:flex-row items-center justify-center my-6 space-y-4 md:space-y-0 md:space-x-4  rounded  max-w-md md:max-w-3xl mx-auto p-3 bg-white shadow-sm shadow-gray-300 text-black">
+            <div className="border-2 border-orange-500 rounded-xl flex flex-col md:flex-row items-center justify-center my-6 space-y-4 md:space-y-0 md:space-x-4    max-w-md md:max-w-3xl mx-auto p-3 bg-gray-900   text-white">
                 {user.profilePicture && (
                     <Image
                         src={user.profilePicture}
                         alt="Profile"
                         width={100}
                         height={100}
-                        className="w-24 h-24 rounded-full border border-gray-600"
+                        onClick={''}
+                        className="w-24 h-24 rounded-full border border-gray-600 cursor-pointer"
                     />
                 )}
                 <div className="text-center md:text-left">
                     <h1 className="text-2xl md:text-3xl font-bold font-amaranth">{user.username || "N/A"}</h1>
-                    <p className="text-lg font-amaranth"><MdOutlineEmail /> {user.email || "N/A"}</p>
+                    <p className="text-lg font-amaranth flex items-center"><MdOutlineEmail /> {user.email || "N/A"}</p>
                 </div>
             </div>
 
             {/* User Stats */}
-            <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-4 border-2 rounded-2xl border-orange-500 max-w-md md:max-w-3xl mx-auto p-3 bg-yellow-200 text-black">
+            <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-4 border-2 rounded-2xl border-orange-500 max-w-md md:max-w-3xl mx-auto p-3 bg-gray-900 text-white">
                 <p>
-                    <span className="font-semibold">Created:</span>{" "}
-                    {user.storiesCreated?.length || 0}
+                    <span className="font-semibold flex items-center text-gray-400 text-xl font-amaranth"> <SiStorybook className="text-3xl " color="yellow"/>Stories Written</span>{" "}
+                  
+                  <p className="text-xl font-bold font-pacifico">
+                  {user.storiesCreated?.length || 0}
+                    </p>  
+                   
                 </p>
                 <p>
-                    <span className="font-semibold">Participated:</span>{" "}
+                    <span className="font-semibold flex items-center text-gray-400 text-xl font-amaranth"><TiGroupOutline className="text-3xl mx-1" color="yellow"/>Participated</span>{" "}
+                    <p className="text-xl font-bold font-pacifico">
                     {user.storiesParticipated?.length || 0}
+                        </p>  
                 </p>
                 <p>
-                    <span className="font-semibold">Followers:</span>{" "}
+                    <span className="font-semibold flex items-center text-xl text-gray-400 font-amaranth"><FaPeopleRoof className="text-3xl mx-1" color="yellow"/> Followers</span>{" "}
+                    <p className="text-xl font-bold font-pacifico">
                     {user.followers?.length || 0}
+                        </p>  
                 </p>
                 <p>
-                    <span className="font-semibold">Following:</span>{" "}
+                    <span className="font-semibold flex items-center text-xl text-gray-400 font-amaranth"><FaPeopleGroup className="text-3xl" color="yellow mx-1"/> Following</span>{" "}
+                    <p className="text-xl font-bold font-pacifico">
                     {user.following?.length || 0}
+                        </p>  
                 </p>
             </div>
         </div>
